17f3b561acc8fc3893f44d6d01b130d12d1d7b72
Zach Tindall <zach.tindall@semanticbits.com>
2019-12-19 13:23:34 -0500

    add comments inline to router history service

--- src/app/services/router-history.service.ts
+++ src/app/services/router-history.service.ts
@@ -14,6 +14,7 @@ export class RouterHistoryService {
   constructor(router: Router) {
     router.events
       .pipe(
+        // only include NavigationStart and NavigationEnd events
         filter(
           event =>
             event instanceof NavigationStart || event instanceof NavigationEnd
@@ -21,6 +22,7 @@ export class RouterHistoryService {
         scan<NavigationStart | NavigationEnd, RouterHistory>(
           (acc, event) => {
             if (event instanceof NavigationStart) {
+              // We need to track the trigger, id, and idToRestore from the NavigationStart events
               return {
                 ...acc,
                 event,
@@ -32,19 +34,26 @@ export class RouterHistoryService {
               };
             }
 
+            // NavigationEnd events
             const history = [...acc.history];
             let currentIndex = acc.currentIndex;
 
+            // router events are imperative (router.navigate or routerLink)
             if (acc.trigger === 'imperative') {
+              // remove all events in history that come after the current index
               history.splice(currentIndex + 1);
 
+              // add the new event to the end of the history and set that as our current index
               history.push({ id: acc.id, url: event.urlAfterRedirects });
               currentIndex = history.length - 1;
             }
 
+            // browser events (back/forward) are popstate events
             if (acc.trigger === 'popstate') {
+              // get the history item that references the idToRestore
               const idx = history.findIndex(x => x.id === acc.idToRestore);
 
+              // if found, set the current index to that history item and update the id
               if (idx > -1) {
                 currentIndex = idx;
                 history[idx].id = acc.id;
@@ -69,6 +78,7 @@ export class RouterHistoryService {
             currentIndex: 0
           }
         ),
+        // filter out so we only act when navigation is done
         filter(
           ({ event, trigger }) => event instanceof NavigationEnd && !!trigger
         )
@@ -77,6 +87,7 @@ export class RouterHistoryService {
         const previous = history[currentIndex - 1];
         const current = history[currentIndex];
 
+        // update current and previous urls
         this.previousUrl$.next(previous ? previous.url : null);
         this.currentUrl$.next(current.url);
       });
